f6d8b1c04fc8b7f77865e90f2105fd7a1c6aad56
Love Wessman <lovewessman@gmail.com>
2023-12-19 21:29:44 +0100

    day 14 part 2

--- day1/Assets/day14/day14.cs
+++ day1/Assets/day14/day14.cs
@@ -7,7 +7,18 @@ public class day14 : MonoBehaviour
 {
     [TextArea] public string input;
     private List<List<char>> rockField;
-    private void Start()
+    public int loopdiloops = 100000;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("le tap");
+            RunIt();
+        }
+    }
+
+    private void RunIt()
     {
         string[] lines = input.Split("\n");
         rockField = new List<List<char>>();
@@ -19,12 +30,24 @@ public class day14 : MonoBehaviour
                 rockField[i].Add(lines[i][j]);
             }
         }
+
+        List<int> results = new List<int>();
         // Tilt in some direction
-        // TiltNorth();
-        TiltSouth();
-        PrintRocks();
-        int res = CalculateRockValue();
-        Debug.Log(res);
+        for (int i = 0; i < loopdiloops; i++)
+        {
+            TiltNorth();
+            TiltWest();
+            TiltSouth();
+            TiltEast();
+            results.Add(CalculateRockValue());
+        }
+        // Realize that the result becomes cyclic after a while - for me it was a cycle length of 19
+        Debug.Log(List2Str(results));
+    }
+    
+    string List2Str(List<int> str)
+    {
+        return ("[" + string.Join(", ", str.Select(s => $"{s}")) + "]");
     }
 
     int CalculateRockValue()
@@ -147,8 +170,97 @@ public class day14 : MonoBehaviour
     }
     #endregion SOUTH
     
+    #region EAST
     
+    private void TiltEast()
+    {
+        for (int j = rockField[0].Count-1; j >= 0; j--)
+        {
+            for (int i = rockField.Count - 1; i >= 0; i--)
+            {
+                // this index - try to move it up
+                // how far north can we go until we hit a wall or another stone?
+                if (rockField[i][j] == 'O')
+                {
+                    ShiftRight(i,j);
+                    //PrintRocks();
+                }
+                
+            }
+        }
+    }
+
+    void ShiftRight(int i, int j)
+    {
+        for (int k = j; k < rockField[0].Count-1 ; k++)
+        {
+            if (CanGoFurtherEast(i, k))
+            {
+                //Debug.Log($"End of the line: From {i},{j}, we went all the way to {k},{j}!");
+                rockField[i][k+1] = 'O';
+                rockField[i][k] = '.';
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    bool CanGoFurtherEast(int i, int j)
+    {
+        if (j >= rockField[0].Count-1) return false;
+        if (rockField[i][j+1] == '#') return false;
+        else if (rockField[i][j+1] == 'O') return false;
+        return true;
+    }
+    #endregion EAST
     
+    #region WEST
+    
+    private void TiltWest()
+    {
+        for (int j = 0; j < rockField[0].Count; j++)
+        {
+            for (int i = rockField.Count - 1; i >= 0; i--)
+            {
+                // this index - try to move it up
+                // how far north can we go until we hit a wall or another stone?
+                if (rockField[i][j] == 'O')
+                {
+                    ShiftLeft(i,j);
+                    //PrintRocks();
+                }
+                
+            }
+        }
+    }
+
+    void ShiftLeft(int i, int j)
+    {
+        for (int k = j; k >= 0 ; k--)
+        {
+            if (CanGoFurtherWest(i, k))
+            {
+                //Debug.Log($"End of the line: From {i},{j}, we went all the way to {k},{j}!");
+                rockField[i][k-1] = 'O';
+                rockField[i][k] = '.';
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    bool CanGoFurtherWest(int i, int j)
+    {
+        if (j <= 0) return false;
+        if (rockField[i][j-1] == '#') return false;
+        else if (rockField[i][j-1] == 'O') return false;
+        return true;
+    }
+    #endregion WEST
     
     
 }
